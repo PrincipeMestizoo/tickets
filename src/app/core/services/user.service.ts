9fdc128b142fce849061b6ac89b621892e363fd1
Daniel Gonzalez <danielgonzalezsan4567@gmail.com>
2026-08-28 09:38:22 -0500

    fix: array in the getUsers to *ngFor iterate

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { User, UserRole } from '../models/user.model';
 
@@ -11,14 +12,20 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.base);
+    return this.http.get<{ data: User[]; total: number }>(this.base).pipe(
+      map(res => res.data)
+    );
   }
 
   getAgents(): Observable<User[]> {
-    return this.http.get<User[]>(this.base, { params: { role: 'agent' } as any });
+    return this.getUsers().pipe(
+      map(users => users.filter(u => u.role === 'agent'))
+    );
   }
 
   updateRole(userId: string, role: UserRole): Observable<User> {
-    return this.http.patch<User>(`${this.base}/${userId}/role`, { role });
+    return this.http.patch<{ data: User }>(`${this.base}/${userId}/role`, { role }).pipe(
+      map(res => res.data)
+    );
   }
 }
